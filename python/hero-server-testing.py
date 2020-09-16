c3c961f55e89b2e2416fa4c8cad37d10c43d587f
Ricardo Ruiz <Ricardo.w.Ruiz@gmail.com>
2020-09-16 14:48:05 -0700

    added python with the command heroku buildpacks:add --index 2 heroku/python

---
+++ python/hero-server-testing.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 13:30:19 2020
+
+@author: rruiz26
+"""
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint  
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("Testing").sheet1
+
+insertRow = ["hello","this","is inserted"]
+
+sheet.insert_row(insertRow,1)
+
+
+# todo for project 
+
+#get this python code (with adaptive experminet code) to run when there is a post to the server 
+
+
+#notes on zapier integration make column/row called newest subsscriber
