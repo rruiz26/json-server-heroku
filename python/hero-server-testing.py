6d05b950d84a1de14a8afb90f4da0d886db2ffcf
Ricardo Ruiz <Ricardo.w.Ruiz@gmail.com>
2020-09-16 18:49:02 -0700

    testing chatfuel/server/googlesheet interaction

--- python/hero-server-testing.py
+++ python/hero-server-testing.py
@@ -6,12 +6,20 @@ Created on Tue Sep 15 13:30:19 2020
 """
 
 import gspread
+import json
 
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 dir =  os.getcwd()
 #from pprint import pprint  
+with open("./db.json","r") as read_file:
+    dict = json.load(read_file)
 
+first_name = dict['users'][-1]['first name']
+last_name = dict['users'][-1]['last name']
+gender = dict['users'][-1]['gender']    
+
+    
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(dir +"/python/creds.json", scope)
@@ -20,7 +28,7 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Testing").sheet1
 
-insertRow = ["hello","this","is", "from", "the", "Heroku", "server"]
+insertRow = [first_name,last_name,gender]
 
 sheet.insert_row(insertRow,1)
 
